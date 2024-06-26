7d75ec248e1f80f0a15b4763060bf9a462b8c75c
viettranni <vttranviett@gmail.com>
2024-06-26 22:23:27 +0300

    Created the library for calculations to better structure the code.

---
+++ calculations.py
@@ -0,0 +1,14 @@
+# Calculations library for calculator
+
+def add(x, y):
+    return x + y
+
+def substract(x,y):
+    return x - y
+
+def multiply(x,y):
+    return x * y
+
+def divide(x,y):
+    return x/y
+
--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("Hello world, this is my calculator in python")
+import calculations
+
+
+def options():
+    print("What would you like to do?\n"
+        "Option 1: Add\n"
+        "Option 2: Subtract\n"
+        "Option 3: Multiply\n"
+        "Option 4: Divide\n")
+
+
+def main():
+    print("Welcome to the 'More than a Calculator'")
+
+    first_input = input("Give me the first number: ")
+    second_input = input("Give me the second number: ")
+
+    options()
+
+    add = add(first_input, second_input)
+    print(add)
+
+
+
